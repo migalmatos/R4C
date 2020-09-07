577e36d42b735b96629b66e00fdbacf7cbbeede9
migalmatos <mig.almatos@gmail.com>
2020-09-07 11:11:11 +0100

    first commit

---
+++ convert_gui.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+from time import sleep
+
+input_file = sys.argv[1:]  # the first argument is the script itself
+
+def convert(input_file):
+    output_file = input_file[0].replace('.ui', '.py')
+    subprocess.call(["pyuic5", "-x", input_file, "-o", output_file])
+    print ('INPUT:', input_file[0], '\nOUTPUT:', output_file)
+    sleep(2)
+
+convert(input_file)
